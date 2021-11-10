c61e541e4b565c28c1b69ec01de0dc63a8083996
thinkverse <hallberg.kim@gmail.com>
2021-11-10 15:24:06 +0100

    Fix company name not being unique

--- database/migrations/task6/2021_11_09_083843_create_companies_table.php
+++ database/migrations/task6/2021_11_09_083843_create_companies_table.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCompaniesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         // TASK: edit this migration so there couldn't be two companies with the same name
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
@@ -26,8 +26,8 @@ class CreateCompaniesTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('companies');
     }
-}
+};
