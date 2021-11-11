da9a80a16fe942dd70c3683557b5752f773e5484
Jeremiah John Boateng <jeremiahjohnboateng@gmail.com>
2021-11-11 16:18:52 +0000

    complete task3

--- database/migrations/task3/2021_11_09_080955_create_projects_table.php
+++ database/migrations/task3/2021_11_09_080955_create_projects_table.php
@@ -17,7 +17,6 @@ class CreateProjectsTable extends Migration
             $table->id();
             $table->string('name');
             $table->timestamps();
-
             $table->softDeletes();
         });
     }
