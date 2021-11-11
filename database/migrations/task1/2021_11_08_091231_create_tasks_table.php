6f6c88599b9251a55805a8f85a9b6d9b6d20b7b3
Lasse Larsen <justlasse@gmail.com>
2021-11-11 10:35:33 +0100

    Updated migrations

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -15,8 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id');
             $table->string('name');
             $table->timestamps();
         });
