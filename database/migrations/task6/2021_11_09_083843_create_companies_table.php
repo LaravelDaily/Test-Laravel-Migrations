2f3b39999743b0e7e53ec0fd4d5b95ce70844708
amir <adada5116@gmail.com>
2021-11-11 11:55:38 +0330

    completed

--- database/migrations/task6/2021_11_09_083843_create_companies_table.php
+++ database/migrations/task6/2021_11_09_083843_create_companies_table.php
@@ -18,6 +18,7 @@ class CreateCompaniesTable extends Migration
             $table->id();
             $table->string('name');
             $table->timestamps();
+            $table->primary('name');
         });
     }
 
