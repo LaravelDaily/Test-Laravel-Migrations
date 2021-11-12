ea595d8bc2a719a12ad070fb83cbcdb1758353c1
Romal Tandel <tandelromal@gmail.com>
2021-11-12 16:31:19 +0530

    Laravel Migration Skil Test Submit

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -15,10 +15,10 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id');
+            $table->bigInteger('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedInteger('comment_id');
-            $table->foreign('comment_id')->references('id')->on('comments');
+            $table->bigInteger('task_id')->unsigned()->index();
+            $table->foreign('task_id')->references('id')->on('tasks');
             $table->string('comment_text');
             $table->timestamps();
         });
