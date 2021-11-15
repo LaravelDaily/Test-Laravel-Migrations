37a8277230f48678956e5c3a8bd45330a5215b69
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2021-11-15 21:45:16 +0200

    it took 30 minutes

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -16,11 +16,9 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-
-    $table->foreign('user_id')->references('id')->on('users');
-    $table->unsignedBigInteger('Task_id');
-
-    $table->foreign('Task_id')->references('id')->on('Tasks');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('comment_id');
+            $table->foreign('comment_id')->references('id')->on('tasks');
             $table->string('comment_text');
             $table->timestamps();
         });
