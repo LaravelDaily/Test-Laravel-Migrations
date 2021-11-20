efde25a642e1a1903bb20dc354362e78045403c4
Giga <gigagotso@gmail.com>
2021-11-21 01:01:07 +0400

    Fixed Database Migrations

--- database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
+++ database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
@@ -14,6 +14,7 @@ class AddSurnameToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->string('surname')->after('name');
             // TASK: Add a string field "surname" which would go after the field "name"
             // Write code here
         });
