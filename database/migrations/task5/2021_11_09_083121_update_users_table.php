c387fcb22c6bf79c47ed715f96962845eac72774
erik.masny <erik.masny@gmail.com>
2021-11-26 23:06:47 +0100

    Complete all tasks

--- database/migrations/task5/2021_11_09_083121_update_users_table.php
+++ database/migrations/task5/2021_11_09_083121_update_users_table.php
@@ -14,9 +14,11 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         // TASK: add an if-statement in this file to NOT add column if it already exists
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('name');
-        });
+        if( ! Schema::hasColumn('users', 'name') ) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('name');
+            });
+        }
     }
 
     /**
