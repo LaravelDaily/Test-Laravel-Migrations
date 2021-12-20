6b6be4e7f1e1f40400806f4257b35cdb2f11d4d1
codejutsu1 <codejutsu@protonmail.com>
2021-12-20 14:12:40 +0100

    Challenge complete

--- tests/Feature/MigrationsTest.php
+++ tests/Feature/MigrationsTest.php
@@ -16,7 +16,7 @@ use Tests\TestCase;
 class MigrationsTest extends TestCase
 {
     // !!! IMPORTANT: TESTS ARE CONFIGURED TO RUN ON A LOCAL MYSQL DATABASE
-    //     WHICH SHOULD BE CALLED "mysql_testing"
+    //     WHICH SHOULD BE CALLED " mysql_testing"
     // !!! DON'T FORGET TO CREATE THAT DATABASE
 
     // !!! NOTICE: THAT DB WILL BE WIPED A LOT WITHIN TESTS BY "migrate:fresh"
