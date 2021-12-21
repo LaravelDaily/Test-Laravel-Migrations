507f4ccec54a1e673a1ec6c308660839496b912b
Hina <92708031+Hina326@users.noreply.github.com>
2021-12-21 17:08:03 +0500

    Update 2021_11_09_083121_update_users_table.php

--- database/migrations/task5/2021_11_09_083121_update_users_table.php
+++ database/migrations/task5/2021_11_09_083121_update_users_table.php
@@ -13,10 +13,14 @@ class UpdateUsersTable extends Migration
      */
     public function up()
     {
-        // TASK: add an if-statement in this file to NOT add column if it already exists
         Schema::table('users', function (Blueprint $table) {
             $table->string('name');
         });
+        if(!Schema::hasColumn('users', 'email')){
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('name');
+            });
+        }
     }
 
     /**
@@ -26,6 +30,41 @@ class UpdateUsersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('users');
+    }
+}
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name');
+        });
+        if(!Schema::hasColumn('users', 'email')){
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('name');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
     }
 }
