3714fac0d75afc2ec308488b70852719aea121f7
Gleydson Silva <ghudson.ti@gmail.com>
2022-01-21 18:16:49 +0100

    All task done! 30 min.

--- database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
+++ database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
@@ -16,6 +16,9 @@ class AddSurnameToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             // TASK: Add a string field "surname" which would go after the field "name"
             // Write code here
+            $table->after('surname', function ($table){
+                $table->string('surname');
+            });
         });
     }
 
