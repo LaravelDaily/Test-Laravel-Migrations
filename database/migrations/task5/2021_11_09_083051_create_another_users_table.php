24ff76a7c6bbc8d3fb1874a4b765281cfd6e84dd
neo <neomn110@gmail.com>
2022-02-27 17:39:12 +0330

    task 2 3 5 6 done , task 4 unresolved yet

--- database/migrations/task5/2021_11_09_083051_create_another_users_table.php
+++ database/migrations/task5/2021_11_09_083051_create_another_users_table.php
@@ -13,15 +13,16 @@ class CreateAnotherUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users'))
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
     }
 
     /**
