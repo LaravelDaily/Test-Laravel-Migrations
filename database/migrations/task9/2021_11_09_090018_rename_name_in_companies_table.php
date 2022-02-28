65a99cbec63ee16edfa16f2eddd7db4e98b20aa5
Nat <nathaniel.ong@capbay.com>
2022-02-28 17:37:46 +0800

    First try

--- database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
+++ database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
@@ -15,7 +15,7 @@ class RenameNameInCompaniesTable extends Migration
     {
         // TASK: write the migration to rename the column "title" into "name"
         Schema::table('companies', function (Blueprint $table) {
-            // Write code here
+            Schema::renameColumn('title', 'name');
         });
     }
 
@@ -28,6 +28,7 @@ class RenameNameInCompaniesTable extends Migration
     {
         Schema::table('companies', function (Blueprint $table) {
             //
+            Schema::renameColumn('name', 'title');
         });
     }
 }
