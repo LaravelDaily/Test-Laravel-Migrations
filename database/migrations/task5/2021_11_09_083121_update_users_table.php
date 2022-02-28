891b2da0734d804b0ef9d36fb3b963ea33aef4e9
Nat <nathaniel.ong@capbay.com>
2022-02-28 17:42:45 +0800

    Second try

--- database/migrations/task5/2021_11_09_083121_update_users_table.php
+++ database/migrations/task5/2021_11_09_083121_update_users_table.php
@@ -14,7 +14,7 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         // TASK: add an if-statement in this file to NOT add column if it already exists
-        if(!Schema::hasColumn('name')) {
+        if(!Schema::hasColumn('users', 'name')) {
             Schema::table('users', function (Blueprint $table) {
                 $table->string('name');
             });
