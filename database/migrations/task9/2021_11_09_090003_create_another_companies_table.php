1f0b2ad7a929d35583509d167287a67e06288461
Nat <nathaniel.ong@capbay.com>
2022-02-28 17:45:30 +0800

    Third try

--- database/migrations/task9/2021_11_09_090003_create_another_companies_table.php
+++ database/migrations/task9/2021_11_09_090003_create_another_companies_table.php
@@ -15,7 +15,7 @@ class CreateAnotherCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->timestamps();
         });
     }
