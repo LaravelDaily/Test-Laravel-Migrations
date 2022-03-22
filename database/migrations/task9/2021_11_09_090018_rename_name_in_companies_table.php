f9589bda09b52405950566a1656cc01ee836f3e7
Luca Di Fazio <highliuk@gmail.com>
2022-03-22 23:16:52 +0100

    ✅ Complete Task 9

--- database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
+++ database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
@@ -13,9 +13,8 @@ class RenameNameInCompaniesTable extends Migration
      */
     public function up()
     {
-        // TASK: write the migration to rename the column "title" into "name"
         Schema::table('companies', function (Blueprint $table) {
-            // Write code here
+            $table->renameColumn('title', 'name');
         });
     }
 
@@ -27,7 +26,7 @@ class RenameNameInCompaniesTable extends Migration
     public function down()
     {
         Schema::table('companies', function (Blueprint $table) {
-            //
+            $table->renameColumn('name', 'title');
         });
     }
 }
