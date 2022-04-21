5219a8ab2edf97a114ccec84a691e0373f5287f5
Santhi G <101110649+santhi-radiant@users.noreply.github.com>
2022-04-21 07:08:53 -0700

    Update 2021_11_09_085453_rename_companies_table.php

--- database/migrations/task8/2021_11_09_085453_rename_companies_table.php
+++ database/migrations/task8/2021_11_09_085453_rename_companies_table.php
@@ -14,7 +14,9 @@ class RenameCompaniesTable extends Migration
     public function up()
     {
         // TASK: add a migration to rename table "company" into "companies"
-        Schema::rename('company', 'companies');
+         Schema::table('company', function(Blueprint $table) {
+            $table->rename('companies');
+        });
     }
 
     /**
