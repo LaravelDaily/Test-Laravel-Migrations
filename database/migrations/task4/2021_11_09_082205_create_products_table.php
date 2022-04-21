4d47cbac3f4e8d264b8edcfee5771c4d0665ecc0
Santhi G <101110649+santhi-radiant@users.noreply.github.com>
2022-04-21 06:04:15 -0700

    Update 2021_11_09_082205_create_products_table.php

--- database/migrations/task4/2021_11_09_082205_create_products_table.php
+++ database/migrations/task4/2021_11_09_082205_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         // TASK: Edit this file, so that deleting category would auto-delete its products
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->string('name');
             $table->timestamps();
         });
