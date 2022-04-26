4705a4e7da8e455dab9170e3efa803c1527269b4
David Villalobos <davidvillalobosvargas@gmail.com>
2022-04-26 14:45:15 -0600

    test 2

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -15,10 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users');
-            // $table->unsignedInteger('comment_id');
-            // $table->foreign('comment_id')->references('id')->on('comments');
             $table->string('comment_text');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('comment_id')->constrained('comments');
             $table->timestamps();
         });
     }
