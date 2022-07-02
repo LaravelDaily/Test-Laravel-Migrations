5307ac76a6032bd99b27539ace7fb25be1c346b0
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-07-02 14:26:55 +0700

    task completed

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -15,8 +15,10 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            // $table->bigInteger('user_id');
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('name');
             $table->string('name');
             $table->timestamps();
         });
--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -15,10 +15,12 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedInteger('comment_id');
-            $table->foreign('comment_id')->references('id')->on('comments');
+            // $table->unsignedInteger('user_id');
+            // $table->foreign('user_id')->references('id')->on('users');
+            // $table->unsignedInteger('comment_id');
+            // $table->foreign('comment_id')->references('id')->on('comments');
+            // $table->string('comment_text');
+            $table->foreignId('user_id')->constrained('users');
             $table->string('comment_text');
             $table->timestamps();
         });
