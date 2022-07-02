43e8ead34b82f405e70e41fc424a420f6f3cf79a
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-07-02 14:34:07 +0700

    task completed

--- database/migrations/task4/2021_11_09_082205_create_products_table.php
+++ database/migrations/task4/2021_11_09_082205_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             // $table->foreignId('category_id')->constrained();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->timestamps();
         });
