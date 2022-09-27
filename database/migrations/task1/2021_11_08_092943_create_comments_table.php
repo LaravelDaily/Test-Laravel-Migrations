f3d5a59d6b59141609eed4fdf34fed0af45b70aa
ErikasAbukauskas <85123397+ErikasAbukauskas@users.noreply.github.com>
2022-09-28 00:03:58 +0300

    Update 2021_11_08_092943_create_comments_table.php

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -15,10 +15,11 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id');
+            //$table->unsignedInteger('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedInteger('comment_id');
-            $table->foreign('comment_id')->references('id')->on('comments');
+            //$table->unsignedInteger('comment_id');
+            //$table->foreign('comment_id')->references('id')->on('comments');
             $table->string('comment_text');
             $table->timestamps();
         });
