5fd68f4088cda5832859be648d2682da4349e242
ErikasAbukauskas <85123397+ErikasAbukauskas@users.noreply.github.com>
2022-09-28 00:04:39 +0300

    Update 2021_11_08_091231_create_tasks_table.php

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -15,7 +15,6 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            //$table->bigInteger('user_id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
