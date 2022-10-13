a6bcfb1a04c23d08de6367e104d88e1ba547608c
Khaled Ahmed <36667919+KhaledTolba@users.noreply.github.com>
2022-10-13 14:50:19 +0200

    Update 2021_11_09_090018_rename_name_in_companies_table.php

--- database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
+++ database/migrations/task9/2021_11_09_090018_rename_name_in_companies_table.php
@@ -30,7 +30,6 @@ class RenameNameInCompaniesTable extends Migration
     {
         Schema::table('companies', function (Blueprint $table) {
             //
-            $table->renameColumn('name','title');
         });
     }
 }
