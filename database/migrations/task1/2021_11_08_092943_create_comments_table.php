2b5d79f52d5a2834858e1475be595d363e754130
Timur <timurcool650@gmail.com>
2022-10-28 10:00:31 +0600

    tasks

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -17,6 +17,8 @@ class CreateCommentsTable extends Migration
             $table->id();
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('comment_id');
+            $table->foreign('comment_id')->references('id')->on('comments');
             $table->string('comment_text');
             $table->timestamps();
         });
