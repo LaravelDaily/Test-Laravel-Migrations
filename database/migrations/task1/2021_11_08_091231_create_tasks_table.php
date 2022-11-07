0b51cafc96d4d85ee2112a261697e75498aa4b00
Almir Mazlagic <kevi_almir@hotmail.com>
2022-11-07 15:52:06 +0100

    Test-Laravel-Migrations

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -15,8 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->constrained();
             $table->string('name');
             $table->timestamps();
         });
@@ -32,3 +31,4 @@ class CreateTasksTable extends Migration
         Schema::dropIfExists('tasks');
     }
 }
+            
