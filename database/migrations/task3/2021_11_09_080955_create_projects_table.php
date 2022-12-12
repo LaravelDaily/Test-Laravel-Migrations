6c43663fb5287e25a64c643da1f9d604220f162c
TaweelapTop <117077117+TaweelapTop@users.noreply.github.com>
2022-12-12 17:40:05 +0700

    Update 2021_11_09_080955_create_projects_table.php

--- database/migrations/task3/2021_11_09_080955_create_projects_table.php
+++ database/migrations/task3/2021_11_09_080955_create_projects_table.php
@@ -17,6 +17,7 @@ class CreateProjectsTable extends Migration
             $table->id();
             $table->string('name');
             $table->timestamps();
+            $table->softDelete();
 
             // TASK: Add soft deletes column here
         });
