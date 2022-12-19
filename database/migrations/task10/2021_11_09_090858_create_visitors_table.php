f84c1e2f9af638ebc3a1872b42c092a408c3aa88
erikbocil <erikluis3006@gmail.com>
2022-12-20 00:18:30 +0700

    Exercise

--- database/migrations/task10/2021_11_09_090858_create_visitors_table.php
+++ database/migrations/task10/2021_11_09_090858_create_visitors_table.php
@@ -16,7 +16,7 @@ class CreateVisitorsTable extends Migration
         // TASK: edit this migration so country_id would allow NULL values
         Schema::create('visitors', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('country_id')->constrained()->nullable()->default(NULL);
+            $table->foreignId('country_id')->nullable()->constrained();
             $table->string('ip_address');
             $table->timestamps();
         });
