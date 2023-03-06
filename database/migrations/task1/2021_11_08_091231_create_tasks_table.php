e851a2cc5e7f2d6edbf724696625597494fd7e78
gofran04 <docreader04@gmail.com>
2023-03-07 00:42:15 +0200

    my migration challenge

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -13,12 +13,16 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->bigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->string('name');
-            $table->timestamps();
+        if(!schema::hasTable('tasks')){
+            Schema::create('tasks', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->timestamps();
+            });
+        }   
+        schema::table('tasks',  function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+
         });
     }
 
