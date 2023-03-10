f57d92405a90a363dcff7bf6fbeff5774a105802
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2023-03-09 22:18:49 -0300

    Fix Task 2

--- database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
+++ database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
@@ -26,7 +26,7 @@ class AddSurnameToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->removeColumn('surname');
         });
     }
 }
