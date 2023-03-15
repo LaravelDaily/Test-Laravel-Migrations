f374d90834554cad007c2b4505c39e3a3773d519
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-03-15 15:23:58 +0100

    Task 2 ✅

--- database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
+++ database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
@@ -6,28 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 class AddSurnameToUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('users', function (Blueprint $table) {
-            // TASK: Add a string field "surname" which would go after the field "name"
-            // Write code here
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function (Blueprint $table) {
+			// TASK: Add a string field "surname" which would go after the field "name"
+			// Write code here
+			$table->string('surname')->after('name');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function (Blueprint $table) {
+			//
+		});
+	}
 }
