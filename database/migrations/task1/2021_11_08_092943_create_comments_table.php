c14396b0e9290064c000947e90c9a6dbabc97f92
Muhammed Abdelshafy <57871519+Xafy@users.noreply.github.com>
2023-03-20 00:40:04 +0000

    first commit

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -15,7 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedInteger('comment_id');
             $table->foreign('comment_id')->references('id')->on('comments');
