02cbaee9e93769bb0446798dfd44a03e3d135ac8
Mohammad Alkurdi <mohammadkurdieng@gmail.com>
2023-03-22 22:38:19 +0300

    Migrations

--- database/migrations/task10/2021_11_09_090752_create_countries_table.php
+++ database/migrations/task10/2021_11_09_090752_create_countries_table.php
@@ -15,7 +15,7 @@ class CreateCountriesTable extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->default(null);
+            $table->string('name');
             $table->timestamps();
         });
     }
