b69744311b479d6a60dc8d3b965e42a058bde405
davidcabot <117016216+davidcabot@users.noreply.github.com>
2023-04-28 10:06:15 +0300

    all tests passed

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -15,10 +15,10 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedInteger('comment_id');
-            $table->foreign('comment_id')->references('id')->on('comments');
+            $table->foreignId('user_id')->constrained();
+//            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('comment_id')->constrained();
+//            $table->foreign('comment_id')->references('id')->on('comments');
             $table->string('comment_text');
             $table->timestamps();
         });
