3f555f09ffb87750d1ff8ee5ca8f312283ead737
lucidlynxx <dzakysyahrizal04@gmail.com>
2023-05-13 07:06:59 +0700

    second test

--- database/migrations/task5/2021_11_09_083121_update_users_table.php
+++ database/migrations/task5/2021_11_09_083121_update_users_table.php
@@ -13,11 +13,12 @@ class UpdateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('users');
         // TASK: add an if-statement in this file to NOT add column if it already exists
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('name');
-        });
+        if (!Schema::hasColumn('users', 'name')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('name');
+            });
+        }
     }
 
     /**
--- database/migrations/task5/2021_11_09_083225_recreate_users_table.php
+++ database/migrations/task5/2021_11_09_083225_recreate_users_table.php
@@ -13,17 +13,18 @@ class RecreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('users');
         // TASK: add an if-statement in this file to NOT create table if it already exists
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
