ef05d4f219a16680d73e95a6aec91e2ec29e71d9
Anton Cherednichenko <anton.c@globedreamers.com>
2023-05-18 20:01:03 +0200

    completed tasks - ~10 minutes

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id');
+            $table->unsignedbigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
             $table->timestamps();
