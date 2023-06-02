95fd94375598d1e6179534f45d347e2eab77fcbf
mike <mghossain.42@gmail.com>
2023-06-02 09:23:53 +0300

    test successful

--- database/migrations/task8/2021_11_09_085453_rename_companies_table.php
+++ database/migrations/task8/2021_11_09_085453_rename_companies_table.php
@@ -14,6 +14,7 @@ class RenameCompaniesTable extends Migration
     public function up()
     {
         // TASK: add a migration to rename table "company" into "companies"
+        Schema::rename('company', 'companies');
     }
 
     /**
@@ -23,6 +24,6 @@ class RenameCompaniesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('companies');
     }
 }
