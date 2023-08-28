ff475a50aa482071d8c2cdd1233b3b653e4da318
abbas mastan <abbasmastan2424@gmail.com>
2023-08-28 14:06:47 +0500

    tasks completed

--- database/migrations/task5/2021_11_09_083051_create_another_users_table.php
+++ database/migrations/task5/2021_11_09_083051_create_another_users_table.php
@@ -13,6 +13,8 @@ class CreateAnotherUsersTable extends Migration
      */
     public function up()
     {
+        if (!Schema::hasTable('users')) {
+
             Schema::create('users', function (Blueprint $table) {
                 $table->id();
                 $table->string('name');
@@ -22,6 +24,7 @@ class CreateAnotherUsersTable extends Migration
                 $table->rememberToken();
                 $table->timestamps();
             });
+        }
     }
 
     /**
