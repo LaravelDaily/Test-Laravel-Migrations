2aabb97f239e702d033bf6a6e1c509645ab77c10
Samsun Maarif <samsunmaarif62@gmail.com>
2023-09-13 09:47:31 +0700

    update

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -15,7 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('comment_text');
             $table->timestamps();
