f20f2d300727e849be45c16b24d31604230f549f
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2023-10-20 14:50:03 +0600

    making the tasks

--- database/migrations/task5/2021_11_09_083121_update_users_table.php
+++ database/migrations/task5/2021_11_09_083121_update_users_table.php
@@ -13,10 +13,12 @@ class UpdateUsersTable extends Migration
      */
     public function up()
     {
-        // TASK: add an if-statement in this file to NOT add column if it already exists
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('name');
-        });
+        // TASK: add an if-statement in this file to NOT add column if it already exists'
+        if (!Schema::hasColumn('users', 'name')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('name');
+            });
+        }
     }
 
     /**
@@ -26,6 +28,6 @@ class UpdateUsersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropColumns('users', ['name']);
     }
 }
