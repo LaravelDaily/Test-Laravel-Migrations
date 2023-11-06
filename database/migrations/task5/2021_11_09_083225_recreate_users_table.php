961527abdc441b3f073ad1a61ecb09c730a9f1ab
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-11-06 18:08:56 +0800

    Update 2021_11_09_083225_recreate_users_table.php

--- database/migrations/task5/2021_11_09_083225_recreate_users_table.php
+++ database/migrations/task5/2021_11_09_083225_recreate_users_table.php
@@ -14,7 +14,8 @@ class RecreateUsersTable extends Migration
     public function up()
     {
         // TASK: add an if-statement in this file to NOT create table if it already exists
-        Schema::create('users', function (Blueprint $table) {
+        if(Schema::hasTable('users')==false){
+            Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -23,6 +24,7 @@ class RecreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+        }
     }
 
     /**
