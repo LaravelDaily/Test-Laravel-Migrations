1c7d7a0e1994bff83a5afd0eb085485994d964c9
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-11-13 18:18:17 +0800

    Update 2021_11_09_085453_rename_companies_table.php

--- database/migrations/task8/2021_11_09_085453_rename_companies_table.php
+++ database/migrations/task8/2021_11_09_085453_rename_companies_table.php
@@ -14,6 +14,8 @@ class RenameCompaniesTable extends Migration
     public function up()
     {
         // TASK: add a migration to rename table "company" into "companies"
+        Schema::rename('company','companies');
+        
     }
 
     /**
@@ -24,5 +26,6 @@ class RenameCompaniesTable extends Migration
     public function down()
     {
         //
+        Schema::rename('companies'.'company');
     }
 }
