03cf31c8ff708fff6ea0d4d2f7b8160bb0c961c6
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-11-19 22:28:56 +0800

    Update 2021_11_09_075928_add_surname_to_users_table.php

--- database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
+++ database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
@@ -28,6 +28,7 @@ class AddSurnameToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->string('surname')->after('name');
         });
     }
 }
