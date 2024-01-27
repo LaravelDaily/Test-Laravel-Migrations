49a19b5cfdfad735108b6cc67c3da4d8ee73a940
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-01-27 20:15:20 +0800

    Update 2021_11_08_092943_create_comments_table.php

--- database/migrations/task1/2021_11_08_092943_create_comments_table.php
+++ database/migrations/task1/2021_11_08_092943_create_comments_table.php
@@ -16,8 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->unsignedInteger('comment_id');
-            $table->foreign('comment_id')->references('id')->on('comments');
+            $table->foreignId('comment_id')->constrained()->cascadeOnDelete();
             $table->string('comment_text');
             $table->timestamps();
         });
