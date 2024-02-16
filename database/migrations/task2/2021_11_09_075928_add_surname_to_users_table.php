4a7b5799649e2df1740ee314b375bb22700afc99
mohamed gamal <mohamedhussin07@gmail.com>
2024-02-16 14:32:49 +0200

    test

--- database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
+++ database/migrations/task2/2021_11_09_075928_add_surname_to_users_table.php
@@ -28,7 +28,7 @@ class AddSurnameToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('surname');
         });
     }
 }
