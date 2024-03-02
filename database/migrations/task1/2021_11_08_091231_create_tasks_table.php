ada9ac4c61161df5c42905a7d0b1d6f3ee01cf0f
Don <149981944+Dong33333@users.noreply.github.com>
2024-03-02 21:18:07 +0800

    Update 2021_11_08_091231_create_tasks_table.php

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -15,8 +15,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+    
             $table->string('name');
             $table->timestamps();
         });
